91c8d11bb9143510ffaf673f6a6277868d50a778
Gennady Gurov <ggurov@gmail.com>
2025-07-08 12:04:44 -0400

    latest

--- keyboard_basic1/keyboard_basic1.ino
+++ keyboard_basic1/keyboard_basic1.ino
@@ -1,8 +1,7 @@
 #include <EspUsbHost.h>
 #include "EspUsbHost.h"
 #include <ESP32-TWAI-CAN.hpp>
-#include <Arduino_CRC32.h>
-Arduino_CRC32 crc32;
+
 unsigned long tick = 0;
 unsigned long last_tick = 0;
 int gone = 1;
@@ -13,56 +12,52 @@ unsigned long notWorking = 0;
 CanFrame rxobdFrame         = {0};
 CanFrame obdFrame         = {0};
 
-void sendTSCMD(uint8_t modifier, uint8_t firstKey, uint8_t secondKey) {
+void sendCMD(uint8_t modifier, uint8_t firstKey, uint8_t secondKey) {
+  int retry = 5;
+  while (retry > 0) {
+    if (ESP32Can.canState()) break;
+    delay(13);
+    Serial.println("retry");
+    retry--;
+  }
+
   if (ESP32Can.canState() != 1) { 
     Serial.println("Can isn't working ?");
     notWorking++;
     return;
   }
+
     uint32_t crc32_res;
     uint8_t payload[5];
-//    CanFrame obdFrame         = {0};
-    obdFrame.identifier       = 0x710; // serial write;
-    obdFrame.extd             = 0;
-    obdFrame.data_length_code = 3;
-    obdFrame.data[0]          = 0x02; // length of message to follow
-    obdFrame.data[1]          = 0x00;
-    obdFrame.data[2]          = 0x05;
-  ESP32Can.writeFrame(obdFrame, 0);
-
  
     payload[0] = 0x5A;
     payload[1] = 0;
     payload[2] = 27;
     payload[3] = secondKey & 0xff;
     payload[4] = firstKey & 0xff;
-    crc32_res = crc32.calc(payload, 5);
 
     Serial.printf("sending %02x %02x", secondKey, firstKey);
     Serial.println();
   
-    obdFrame.identifier       = 0x710; // serial write;
-    obdFrame.extd             = 0;
-    obdFrame.data_length_code = 6;
-    obdFrame.data[0]          = 0x05; // length of message to follow
-    obdFrame.data[1]          = payload[0]; // Z - test buttons
-    obdFrame.data[2]          = payload[1];    // TS_BUTTONBOX1_CATEGORY = 26, 0x00 0x1A
-    obdFrame.data[3]          = payload[2]; // hardware button box 1 -> lookup on ECU side via lookup curve table thing; 
-    obdFrame.data[4]          = payload[3]; // data 
-    obdFrame.data[5]          = payload[4];  // data
-    ESP32Can.writeFrame(obdFrame, 0);
-
-    obdFrame.identifier       = 0x710; // serial write;
+    obdFrame.identifier       = 0x711; // CANBUS BUTTONBOX
     obdFrame.extd             = 0;
     obdFrame.data_length_code = 5;
-    obdFrame.data[0]          = 4;
-    obdFrame.data[1]          = (crc32_res >> 24) & 0xff; // crc high bits
-    obdFrame.data[2]          = (crc32_res >> 16) & 0xff;
-    obdFrame.data[3]          = (crc32_res >> 8)  & 0xff;  
-    obdFrame.data[4]          = (crc32_res     )  & 0xff; // crc low bits
-    
-    ESP32Can.writeFrame(obdFrame, 0);
+    obdFrame.data[0]          = payload[0]; // Z - test buttons
+    obdFrame.data[1]          = payload[1];    // TS_BUTTONBOX1_CATEGORY = 26, 0x00 0x1A
+    obdFrame.data[2]          = payload[2]; // hardware button box 1 -> lookup on ECU side via lookup curve table thing; 
+    obdFrame.data[3]          = payload[3]; // data 
+    obdFrame.data[4]          = payload[4];  // data
 
+    retry=5;
+    while (retry > 0) {
+      if (ESP32Can.writeFrame(obdFrame, 5)) break;
+      Serial.println("retry");
+      retry--;
+    }
+
+    while (ESP32Can.inRxQueue() > 0) {
+      ESP32Can.readFrame(rxobdFrame, 1);
+    }
 }
 
 class MyEspUsbHost : public EspUsbHost {
@@ -91,9 +86,9 @@ class MyEspUsbHost : public EspUsbHost {
 
         // return; // bit of mojibake here
         if (firstKey > 0) {
-          sendTSCMD( modifier,  firstKey & 0xff,  (firstKey >> 8) & 0xff);
+          sendCMD( modifier,  firstKey & 0xff,  (firstKey >> 8) & 0xff);
         } else if (secondKey > 0) {
-          sendTSCMD( modifier,  secondKey & 0xff,  (secondKey >> 8) & 0xff);
+          sendCMD( modifier,  secondKey & 0xff,  (secondKey >> 8) & 0xff);
         }
 
       }
@@ -104,7 +99,7 @@ MyEspUsbHost usbHost;
 
 void setup() {
   Serial.begin(115200);
-  delay(500);
+//  delay(500);
   int i;
   tick = xTaskGetTickCount();
 
@@ -121,15 +116,14 @@ gone = 0;
 }
 
 void loop() {
-  if (ESP32Can.inRxQueue() > 0) {
+  while (ESP32Can.inRxQueue() > 0) {
     ESP32Can.readFrame(rxobdFrame, 0);
-
   }
   if (xTaskGetTickCount() - tick > 1000) {
     tick = xTaskGetTickCount();
 
 
-  Serial.printf("txq: %d, rxq: %d, rxerr: %d. txerr: %d, rxmis: %d, txmis: %d, buserr: %d, canstate: %d, notWorking: %d", 
+/*  Serial.printf("txq: %d, rxq: %d, rxerr: %d. txerr: %d, rxmis: %d, txmis: %d, buserr: %d, canstate: %d, notWorking: %d", 
    ESP32Can.inTxQueue(),
    ESP32Can.inRxQueue(),
    ESP32Can.rxErrorCounter(),
@@ -139,16 +133,13 @@ void loop() {
    ESP32Can.busErrCounter(),
    ESP32Can.canState(), notWorking);
     Serial.println();
+*/
 
-
-  if (notWorking > 10) {
+  if (notWorking > 30 || ESP32Can.busErrCounter() > 30 ) {
     Serial.print(" busErrCounter - restarting esp");
     ESP.restart();
   } 
 }
-
-
-
   usbHost.task();
 }
 
